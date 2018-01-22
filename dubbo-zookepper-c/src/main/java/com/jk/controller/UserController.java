2c8885c44110e2f50714757ade5ea8fa48b737e4
fenglinqiang <839433015@qq.com>
2018-01-22 11:22:56 +0800

    小强的又一次的main4

--- dubbo-zookepper-c/src/main/java/com/jk/controller/UserController.java
+++ dubbo-zookepper-c/src/main/java/com/jk/controller/UserController.java
@@ -120,6 +120,7 @@ public class UserController {
 	public static void main(String[] args) {
 		System.out.println("xxxxxxxxxxxxxxxxx");
 		System.out.println("12345678");
+		System.out.println("1234567234567898");
 		System.out.println("hhhhhhhhhhhh");
 	}
 }
